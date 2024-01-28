0e384364715cc527e311cc764be60aa2a7d69f79
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-01-28 21:46:42 +0100

    Addd template

--- main.py
+++ main.py
@@ -5,4 +5,33 @@ import ttkbootstrap as tb
 root = tb.Window(title="Foodie", themename="superhero", iconphoto="School Apps/Foodie/assets/food.ico")
 root.geometry("%dx%d" % (root.winfo_screenwidth(), root.winfo_screenheight()))
 
+home = tb.Frame(root)
+about = tb.Frame(root)
+
+
+def toHome():
+    about.forget()
+    home.pack()
+
+
+def toAbout():
+    home.forget()
+    about.pack()
+
+
+home.pack()
+label = tb.Label(home, text="Home", font=(
+    "Helvetica", 36), bootstyle="warning")
+label.pack()
+
+button = tb.Button(home, text="To About", bootstyle="success", command=toAbout)
+button.pack(pady=10)
+
+label = tb.Label(about, text="About", font=(
+    "Helvetica", 36), bootstyle="warning")
+label.pack()
+
+button = tb.Button(about, text="To Home", bootstyle="success", command=toHome)
+button.pack(pady=10)
+
 root.mainloop()
